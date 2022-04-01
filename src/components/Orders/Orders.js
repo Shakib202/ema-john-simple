b8cd3fe93254548fa825a12706a8c4f44ed295ba
Shakib202 <shakib2020360@gmail.com>
2022-04-02 05:33:35 +0600

    second commit

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
-import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css';
@@ -10,29 +8,20 @@ import './Orders.css';
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
-    const navigate = useNavigate();
-    const handleRemoveProduct = product =>{
-        const rest = cart.filter(pd => pd.id !== product.id);
-        setCart(rest);
-        removeFromDb(product.id);
-    }
 
     
     return (
         <div className='shop-container'>
-            <div className="review-items-container">
+            <div className='review-items-container'>
                 {
                     cart.map(product => <ReviewItem
-                        key={product.id}
-                        product ={product}
-                        handleRemoveProduct = {handleRemoveProduct}
+                     key={product.id}
+                     product={product}
                     ></ReviewItem>)
                 }
             </div>
-            <div className="cart-container">
-                <Cart cart={cart}>
-                        <button onClick={()=>navigate('/inventory')}>Proceed Checkout </button>
-                </Cart>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
