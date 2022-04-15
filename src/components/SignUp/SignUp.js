c7d83ff94cfb0516e556af31fcf794768c77a753
Shakib202 <shakib2020360@gmail.com>
2022-04-15 18:06:16 +0600

    added signup google button

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import logoPhoto from "../../images/google-logo.png";
 import './SignUp.css'
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
@@ -42,6 +46,19 @@ const SignUp = () => {
     createUserWithEmailAndPassword(email, password);
   }
 
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
+
   return (
     <div className="form-container">
       <div>
@@ -65,6 +82,15 @@ const SignUp = () => {
         <p className="p-tag-style">
           Already have an account? <Link className="form-link" to="/login">Login</Link>
         </p>
+        <div className="or-div">
+          <div className="or-div-line"></div>
+          <div className="or-div-2">or</div>
+          <div className="or-div-line"></div>
+        </div>
+        <button onClick={handleGoogleSignIn} className="google-btn-style">
+          <img src={logoPhoto} alt="" />
+          <p>Continue with Google</p>
+        </button>
       </div>
     </div>
   );
