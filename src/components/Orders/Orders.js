635ab1c8cefb9918438bffe30da8b827aac76ded
Shakib202 <shakib2020360@gmail.com>
2022-05-06 14:37:43 +0600

    add mongodb

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -12,9 +12,9 @@ const Orders = () => {
     const [cart, setCart] = useCart(products);
     const navigate = useNavigate();
     const handleRemoveProduct = product =>{
-        const rest = cart.filter(pd => pd.id !== product.id);
+        const rest = cart.filter(pd => pd._id !== product._id);
         setCart(rest);
-        removeFromDb(product.id);
+        removeFromDb(product._id);
     }
 
     
@@ -23,7 +23,7 @@ const Orders = () => {
             <div className="review-items-container">
                 {
                     cart.map(product => <ReviewItem
-                        key={product.id}
+                        key={product._id}
                         product ={product}
                         handleRemoveProduct = {handleRemoveProduct}
                     ></ReviewItem>)
@@ -87,9 +87,9 @@ export default Orders;
 //     const [cart, setCart] = useCart(products);
 
 //     const handleRemoveProduct = product => {
-//         const rest = cart.filter(pd => pd.id !== product.id);
+//         const rest = cart.filter(pd => pd._id !== product._id);
 //         setCart(rest);
-//         removeFromDb(product.id)
+//         removeFromDb(product._id)
 //     }
 
     
@@ -98,7 +98,7 @@ export default Orders;
 //             <div className='review-items-container'>
 //                 {
 //                     cart.map(product => <ReviewItem
-//                      key={product.id}
+//                      key={product._id}
 //                      product={product}
 //                      handleRemoveProduct={handleRemoveProduct}
 //                     ></ReviewItem>)
